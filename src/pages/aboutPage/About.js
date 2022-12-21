253904d38fe0fd6b4809431de16848b4598e6b76
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2022-12-21 23:30:53 +0530

    style added

--- src/pages/aboutPage/About.js
+++ src/pages/aboutPage/About.js
@@ -7,19 +7,23 @@ import './about.css';
 const About = () => {
   return (
     <>
-      <div className='abt-img-cnt'></div>
-      <ImgComp />
-      <div className='abt-txt-cnt'>
-        <div className='abt-header-cnt'>
-          <h1 className='abt-header-txt'>
-            I aspire to be a Front-end Developer
-          </h1>
+      <div className='abt-cnt'>
+        <div className='abt-img-cnt'>
+          <ImgComp />
         </div>
-        <div className='abt-para-txt'>
-          <p>
-            I am a fresher ECE graduate with quick problem solving skill,
-            looking for a job as a Front-end Developer
-          </p>
+
+        <div className='abt-txt-cnt'>
+          <div className='abt-header-cnt'>
+            <h1 className='abt-header-txt'>
+              I aspire to be a Front-end Developer
+            </h1>
+          </div>
+          <div className='abt-para-txt'>
+            <p>
+              I am a fresher ECE graduate with quick problem solving skill,
+              looking for a job as a Front-end Developer
+            </p>
+          </div>
         </div>
       </div>
     </>
