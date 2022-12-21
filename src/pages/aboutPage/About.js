05cc550ea5b3df82a42585a6c854416480b25c59
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2022-12-21 13:04:35 +0530

    about page created

---
+++ src/pages/aboutPage/About.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import profilePic from '../../images/myPhoto.jpg';
+
+import './about.css';
+
+const About = () => {
+  return (
+    <>
+      <div className='img-cnt'></div>
+      <div>
+        <div>
+          <h1>I aspire to be a Front-end Developer</h1>
+        </div>
+        <div>
+          <p>
+            I am a fresher ECE graduate with quick problem solving skill,
+            looking for a job as a Front-end Developer
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default About;
